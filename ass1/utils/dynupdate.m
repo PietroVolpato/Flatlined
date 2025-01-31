8f70b6006ae48414ab9ffd1d3dde44a49721d8df
AL0alo <lofaroalessio@gmail.com>
2025-01-31 13:22:53 +0100

    Delete ass1 directory

--- ass1/utils/dynupdate.m
+++
@@ -1,4 +0,0 @@
-function Dnew=dynupdate(D,pp,params)
-
-delta = params.chi*(params.phi*ffree(D,params) + (1-params.phi)*fbmi(pp)); 
-Dnew = D + delta;
--- ass1/utils/ffree.m
+++
@@ -1,9 +0,0 @@
-function F=ffree(D,p)
-
-if D<0.5-p.omega
-    F=-sin(pi/(0.5-p.omega)*D);
-elseif D>0.5+p.omega
-    F=sin(pi/(0.5-p.omega)*(D-0.5-p.omega));
-else
-    F=-p.psi*sin(pi/p.omega*(D-0.5));
-end
